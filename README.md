ff4d3c9f784b92bea33925015f38e354b37c428d
Agarwal, Pankaj <2005pank@gmail.com>
2020-01-06 00:20:23 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Microservice using SpringBoot 2.1.11 and Spring greenwich RC2 spring cloud server 
+Microservice using SpringBoot 2.1.11 and Spring greenwich RC2 spring cloud server
+3 microservices limits-service port 8080,currency-exchange-service port 8000,currency-conversion-service port 8100 . The configuration are picked up from local git repo through spring config cloud server"
+[master (root-commit) de061a4] committing 3 microservices limits-service port 8080,currency-exchange-service port 8000,currency-conversion-service port 8100 . The configuration are picked up from local git repo through spring config cloud server 
---
+++ currency-exchange-service/src/main/java/com/pankaj/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,32 @@
+package com.pankaj.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	@Autowired
+	private Environment enviornment;
+	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retreiveExchangeValue(@PathVariable String from,@PathVariable String to) {
+		
+		//ExchangeValue exchangeValue = new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		exchangeValue.setPort(Integer.parseInt(enviornment.getProperty("local.server.port")));
+		
+		return exchangeValue;
+		
+		
+		
+	}
+
+}
