df2018cab9fd49788f2b3fa10ebab77ec08d3792
Agarwal, Pankaj <2005pank@gmail.com>
2020-01-07 12:48:57 +0530

    Implementing Spring Cloud Sleuth in Currency Converter , Currenncy Exchange microservice and zuul api gateway server to assign a request id to each request

--- currency-exchange-service/src/main/java/com/pankaj/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/pankaj/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.pankaj.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Environment enviornment;
 	
@@ -22,7 +27,7 @@ public class CurrencyExchangeController {
 		//ExchangeValue exchangeValue = new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(enviornment.getProperty("local.server.port")));
-		
+		logger.info("{}" ,exchangeValue);
 		return exchangeValue;
 		
 		
